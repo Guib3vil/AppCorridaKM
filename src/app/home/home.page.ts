bc59c38d1b96296b2fd81615210dc3be8c55389c
GuiBaioni <guilhermebaioni35@gmail.com>
2024-03-28 17:30:32 -0300

    App Corrida KM

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  km='';
+  DT=0;
+  VE=0;
+  VM=0;
+
   constructor() {}
 
-}
+
+  Calcular(){
+    this.DT=parseFloat(this.km)*3.50;
+    if (this.DT < 150) {
+      this.VE=(this.DT)*0.25;
+      this.VM=(this.DT)*0.75;
+    }else {
+      this.VE=(this.DT)*0.2;
+      this.VM=(this.DT)*0.8;
+    }
+
+    }
+  }
+
